1af060cc2e7fdfa4bba1beef5a8cbefc0b94d70b
AsyifahZ <101810729+AsyifahZ@users.noreply.github.com>
2024-02-23 21:11:35 +0700

    Update file_syifa.py

---
+++ file_syifa.py
@@ -0,0 +1,13 @@
+def display_nama():
+    print("*" * 30)
+    print("* HAI, KENALIN AKU :*\n")
+    
+    nama = "Nama\t: Asyifah Zalsabilah"
+    prodi = "Prodi\t: Teknik Biomedis"
+    kampus = "Kampus\t: Universitas Airlangga"
+    
+    print(nama)
+    print(prodi)
+    print(kampus)
+    print("*" * 30)
+display_nama()
